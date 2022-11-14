4f06d17c5f39751130d87d43f17ba092a08a220a
Sowmi-05 <85067591+Sowmi-05@users.noreply.github.com>
2022-11-14 11:47:42 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 4/src/contexts/AppContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const navigate = useNavigate();
+
+  const [skills, setSkills] = useState([]);
+
+  const [user, setUser] = useState(null);
+
+  const [showAlert, setShowAlert] = useState(null);
+
+  useEffect(() => {
+    let temp_user = JSON.parse(localStorage.getItem("user"));
+    if (!temp_user) {
+      navigate("/");
+    } else {
+      setUser(temp_user);
+    }
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{ user, setUser, showAlert, setShowAlert, skills, setSkills }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
